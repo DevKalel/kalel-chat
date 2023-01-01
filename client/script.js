1d7b8a28bbe5b3f4ff601fbc0aeb8d30b690fa45
DevKalel <121743227+DevKalel@users.noreply.github.com>
2023-01-01 04:15:59 -0300

    Update script.js

--- client/script.js
+++ client/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://chat-kalel.onrender.com/', {
+    const response = await fetch('https://kalel.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
